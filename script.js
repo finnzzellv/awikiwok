a7a7187cb6330f4cf432bd921aa0b3bdb9dd12fe
finnzzellv <finzellv@gmail.com>
2025-10-05 02:37:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Menampilkan tahun otomatis
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Fungsi tombol
+document.getElementById("contactBtn").addEventListener("click", () => {
+  alert("Kamu bisa hubungi saya di: finn@example.com");
+  });
